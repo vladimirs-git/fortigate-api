b9525e93443631ce008d43b701fdc739f8ab42f6
vprusakovs <vprusakovs@evolutiongaming.com>
2021-11-07 17:16:23 +0200

    0.0.8 fix: policy.py, id > policyid fix: setup.py, url=packet.__url__

--- fortigate_api/__init__.py
+++ fortigate_api/__init__.py
@@ -3,11 +3,11 @@
 from fortigate_api.api import FortigateAPI
 from fortigate_api.fortigate import Fortigate
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __title__ = "fortigate-api"
 __summary__ = "Python package to configure Fortigate (Fortios) devices using REST API"
 __author__ = "Vladimir Prusakov"
 __email__ = "vladimir.prusakovs@gmail.com"
 __url__ = "https://github.com/vladimirs-git/fortigate-api"
-__download_url__ = "https://github.com/vladimirs-git/fortigate-api/archive/refs/tags/0.0.7.tar.gz"
+__download_url__ = "https://github.com/vladimirs-git/fortigate-api/archive/refs/tags/0.0.8.tar.gz"
 __license__ = "MIT"
