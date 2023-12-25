53e820bef0fb63f20904f2b163b882651a2cfa13
Vladimir Prusakov <dzhedaj@gmail.com>
2023-12-25 10:13:47 +0200

    test__base.pybase

--- tests/test__base.py
+++ tests/test__base.py
@@ -2,50 +2,47 @@
 
 import unittest
 
+import pytest
 from requests import Response
 
-from fortigate_api.base import Base
-from tests.helper__tst import NAME1, SLASH, SLASH_, MockFortigate
+from fortigate_api import FortigateAPI
+from tests.helper__tst import NAME1, SLASH, SLASH_
+
+URL_BASE = "api/v2/cmdb/firewall/address/"
 
 
-class Test(MockFortigate):
-    """Base"""
-
-    def setUp(self):
-        """setUp"""
-        super().setUp()
-        self.obj = Base(rest=self.rest, url_obj="api/v2/cmdb/firewall/address/")
-
-    # =========================== helpers ============================
-
-    def test_valid__highest_response(self):
-        """Base._highest_response()"""
-        for status_codes, req in [
-            ([], 200),
-            ([200, 500, 400], 500),
-        ]:
-            responses = []
-            for status_code in status_codes:
-                response = Response()
-                response.status_code = status_code
-                responses.append(response)
-            result_ = self.obj._highest_response(responses)
-            result = result_.status_code
-            self.assertEqual(result, req, msg=f"{status_codes=}")
-
-    def test_valid__quote_url(self):
-        """Base._quote_url()"""
-        url_base = "api/v2/cmdb/firewall/address/"
-        for url, req in [
-            ("", ""),
-            (url_base, url_base),
-            (f"{url_base}{NAME1}", f"{url_base}{NAME1}"),
-            (f"{url_base}{SLASH}", f"{url_base}{SLASH_}"),
-            (SLASH, SLASH),
-        ]:
-            result = self.obj._quote_url(url=url)
-            self.assertEqual(result, req, msg=f"{url=}")
+@pytest.fixture
+def api():
+    """Init FortigateAPI"""
+    return FortigateAPI(host="host")
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("status_codes, expected", [
+    ([], 200),
+    ([200, 200], 200),
+    ([200, 500, 400], 500),
+    ([200, 400, 500], 500),
+])
+def test__highest_response(api: FortigateAPI, status_codes, expected):
+    """Base._highest_response()"""
+    responses = []
+    for status_code in status_codes:
+        response = Response()
+        response.status_code = status_code
+        responses.append(response)
+    response_: Response = api.address._highest_response(responses)
+    actual = response_.status_code
+    assert actual == expected
+
+
+@pytest.mark.parametrize("url, expected", [
+    ("", ""),
+    (URL_BASE, URL_BASE),
+    (f"{URL_BASE}{NAME1}", f"{URL_BASE}{NAME1}"),
+    (f"{URL_BASE}{SLASH}", f"{URL_BASE}{SLASH_}"),
+    (SLASH, SLASH),
+])
+def test__class_to_attr(api: FortigateAPI, url, expected):
+    """Base._quote_url()"""
+    actual = api.address._quote_url(url=url)
+    assert actual == expected
